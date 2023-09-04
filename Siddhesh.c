a7ad6b49ff7d89986ea48d6762196f33b2dbd2ad
mangeshh09 <88280057+mangeshh09@users.noreply.github.com>
2023-09-04 15:55:08 +0530

    Create Siddhesh.c
    nothing

---
+++ Siddhesh.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+	// take Values
+	int array_list[5]={10,20,30,40,50};
+	int i;
+	//display values with its Index No.
+	for(i=0;i<=5;i++){
+		printf("a[%d]:%d\n",i,array_list[i]);
+	}
+}
